51e87b0d73e78d1d760d0d7e0125cb74f2e4597b
ORTEJ089 <jacob.x.ortega.-nd@disney.com>
2019-02-07 00:45:03 +0800

    hard delete on users

--- app/Http/Controllers/LA/EmployeesController.php
+++ app/Http/Controllers/LA/EmployeesController.php
@@ -270,7 +270,8 @@ class EmployeesController extends Controller
 	public function destroy($id)
 	{
 		if(Module::hasAccess("Employees", "delete")) {
-			Employee::find($id)->delete();
+      Employee::find($id)->delete();
+      User::where('context_id', $id)->delete();
 			
 			// Redirecting to index() method
 			return redirect()->route(config('laraadmin.adminRoute') . '.employees.index');
